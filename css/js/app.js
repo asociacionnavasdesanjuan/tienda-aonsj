859943e34f2934f1544e00489fca1906fb4d864c
asociacionnavasdesanjuan <asociacionornitologicanavasdesanjuan@hotmail.com>
2026-08-04 00:17:20 +0200

    Add initial app.js with startup message

---
+++ css/js/app.js
@@ -0,0 +1,6 @@
+// ======================================
+// TIENDA OFICIAL DEL SOCIO
+// Asociación Ornitológica de Navas de San Juan
+// ======================================
+
+console.log("Tienda Oficial del Socio iniciada correctamente.");
