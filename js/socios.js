cbc6318e7f24a6751c1a503167dabd9dfc1bb8b1
asociacionnavasdesanjuan <asociacionornitologicanavasdesanjuan@hotmail.com>
2026-08-05 21:05:51 +0200

    Update fetch path for socios.json

--- js/socios.js
+++ js/socios.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     function cargarSocios(){
 
-        fetch("data/socios.json")
+      fetch("./data/socios.json")
 
         .then(respuesta => respuesta.json())
 
