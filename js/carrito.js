bbf4d3d617407e8be36086afac99647cc78159a7
asociacionnavasdesanjuan <asociacionornitologicanavasdesanjuan@hotmail.com>
2026-08-05 07:18:16 +0200

    Update carrito.js

--- js/carrito.js
+++ js/carrito.js
@@ -5,9 +5,7 @@
 
 let TELEFONO_WHATSAPP = "34640868527";
 let NOMBRE_ASOCIACION = "Asociación Ornitológica de Navas de San Juan";
-mensaje += "%0A💰 *TOTAL: " + totalCarrito.textContent + "*%0A%0A";
-
-mensaje += MENSAJE_FINAL;
+let MENSAJE_FINAL = "Gracias por comprar en la Asociación Ornitológica de Navas de San Juan.";
 
 const configuracionGuardada = localStorage.getItem("configuracionAONSJ");
 
@@ -256,6 +254,9 @@ if(btnWhatsapp){
 const socio = JSON.parse(localStorage.getItem("socio"));
 
 const observaciones = document.getElementById("observaciones").value;
+        
+let mensaje = "";
+        
  mensaje += "🟢 *PEDIDO " + NOMBRE_ASOCIACION.toUpperCase() + "*%0A";  
 mensaje += "━━━━━━━━━━━━━━━━━━%0A%0A";
 mensaje += "👤 *DATOS DEL SOCIO*%0A";
@@ -292,11 +293,12 @@ carrito.forEach(producto => {
 
 mensaje += "%0A💰 *TOTAL: " + totalCarrito.textContent + "*%0A%0A";
 
-
-        window.open(
-            `https://wa.me/${TELEFONO_WHATSAPP}?text=${mensaje}`,
-            "_blank"
-        );
+mensaje += MENSAJE_FINAL;       
+        
+ window.open(
+    `https://wa.me/${TELEFONO_WHATSAPP}?text=${mensaje}`,
+    "_blank"
+);  
 
     }
 
