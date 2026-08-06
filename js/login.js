3378055d6d40f2bbd4197f1707e21b891f1091de
asociacionnavasdesanjuan <asociacionornitologicanavasdesanjuan@hotmail.com>
2026-08-06 20:37:57 +0200

    Implement login functionality with user validation

--- js/login.js
+++ js/login.js
@@ -1,40 +1,64 @@
-alert("ESTE ES EL LOGIN NUEVO");
 document.getElementById("entrar").addEventListener("click", entrar);
+
 
 async function entrar() {
 
+
     const usuario = document.getElementById("usuario").value.trim();
+
     const password = document.getElementById("password").value.trim();
+
     const mensaje = document.getElementById("mensaje");
+
+
 
     try {
 
+
         const respuesta = await fetch("./data/socios.json");
+
 
         const socios = await respuesta.json();
 
+
+
         const socio = socios.find(s =>
+
             s.numero === usuario &&
+
             s.password.toUpperCase() === password.toUpperCase()
+
         );
+
+
 
         if (!socio) {
 
+
             mensaje.style.color = "red";
+
             mensaje.textContent =
             "Número de socio o contraseña incorrectos.";
 
             return;
+
         }
+
+
 
         if (socio.estado !== "ACTIVO") {
 
+
             mensaje.style.color = "red";
+
             mensaje.textContent =
             "Este socio no tiene acceso activo.";
 
             return;
+
         }
+
+
 
         localStorage.setItem(
             "socio",
@@ -42,28 +66,46 @@ async function entrar() {
         );
 
 
+
+        // ENTRADA ADMINISTRADOR
+
         if (
+
             socio.rol === "ADMIN" ||
+
             socio.rol === "ADMIN_PRINCIPAL"
+
         ) {
+
 
             window.location.href = "admin.html";
 
+
         } else {
 
+
+            // ENTRADA SOCIO NORMAL
+
             window.location.href = "tienda.html";
+
 
         }
 
 
+
     } catch(error) {
+
 
         console.error(error);
 
+
         mensaje.style.color = "red";
+
         mensaje.textContent =
         "Error al comprobar los datos.";
 
+
     }
+
 
 }
