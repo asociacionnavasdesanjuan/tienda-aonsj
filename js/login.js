4bb182ab82186366362078bc1a162f285e75804d
asociacionnavasdesanjuan <asociacionornitologicanavasdesanjuan@hotmail.com>
2026-08-07 01:26:24 +0200

    Refactor login function for improved validation

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
 document.getElementById("entrar").addEventListener("click", entrar);
 
+
 async function entrar() {
 
     const usuario = document.getElementById("usuario").value.trim();
     const password = document.getElementById("password").value.trim();
     const mensaje = document.getElementById("mensaje");
+
 
     try {
 
@@ -12,36 +14,65 @@ async function entrar() {
 
         const socios = await respuesta.json();
 
+
         const socio = socios.find(s =>
             s.numero === usuario &&
-            s.password === password
+            s.password.trim().toUpperCase() === password.toUpperCase()
         );
 
+
         if (!socio) {
+
             mensaje.style.color = "red";
-            mensaje.textContent = "Número de socio o contraseña incorrectos.";
+            mensaje.textContent =
+            "Número de socio o contraseña incorrectos.";
+
             return;
         }
 
+
         if (socio.estado !== "ACTIVO") {
+
             mensaje.style.color = "red";
-            mensaje.textContent = "Socio no activo.";
+            mensaje.textContent =
+            "Socio no activo.";
+
             return;
         }
+
 
         localStorage.setItem(
             "socio",
             JSON.stringify(socio)
         );
 
-        window.location.href = "tienda.html";
+
+        // ENTRADA ADMINISTRADORES
+
+        if (
+            socio.rol === "ADMIN" ||
+            socio.rol === "ADMIN_PRINCIPAL"
+        ) {
+
+            window.location.href = "admin.html";
+
+
+        } else {
+
+
+            window.location.href = "tienda.html";
+
+        }
+
 
     } catch(error) {
+
 
         console.error(error);
 
         mensaje.style.color = "red";
-        mensaje.textContent = "Error cargando datos.";
+        mensaje.textContent =
+        "Error cargando datos.";
 
     }
 
