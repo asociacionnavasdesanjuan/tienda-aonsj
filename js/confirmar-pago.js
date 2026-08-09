8702c2413bd7a5489a80d1131b385debfd12e04b
asociacionnavasdesanjuan <asociacionornitologicanavasdesanjuan@hotmail.com>
2026-08-09 15:26:15 +0200

    Add payment confirmation function
    This file contains a function to confirm payments and logs payment details.

---
+++ js/confirmar-pago.js
@@ -0,0 +1,28 @@
+// =====================================
+// CONFIRMAR PAGOS AONSJ
+// =====================================
+
+
+function pagoConfirmado(datosPago){
+
+    console.log("PAGO CONFIRMADO:", datosPago);
+
+
+    /*
+    Aquí en el futuro llegará la confirmación
+    real de la entidad bancaria.
+
+    Ejemplo:
+
+    datosPago = {
+        socio: "000",
+        concepto: "CUOTA",
+        año: "2026",
+        importe: 20,
+        referencia: "XXXX"
+    }
+
+    */
+
+
+}
